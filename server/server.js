e98d365b5ff99c673ce49bceb46d6bdd6ed9f5bd
adrianb-badulescu <adrianb2104@gmail.com>
2020-07-03 15:54:00 +0300

    GET method added

--- server/server.js
+++ server/server.js
@@ -31,9 +31,15 @@ app.post('/todos', (req, res) => {
 });
 
 app.get('/todos', (req, res) => {
-
+    Todo.find()
+    .then((todos) => {
+        res.status(200).send({todos});
+    })
+    .catch((e) => {
+        res.status(400).send(e);
+    })
 });
-
+ 
 
 
 
